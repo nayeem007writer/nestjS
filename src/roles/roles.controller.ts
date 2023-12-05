062dd0135b137fbf7018906340a2a342fc77a67c
nayeem007writer <nayeemahammed853@gmail.com>
2023-12-05 21:22:40 +0600

    role module file update and role controller file req handlers added

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { 
+    Controller, 
+    Get, 
+    Post, 
+    Put, 
+    Body, 
+    Param, 
+    Delete } from '@nestjs/common';
+
+import { RolesService } from './roles.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+    constructor(private readonly roleService: RolesService) {}
+
+    @Post()
+    create(Body() createRoleDto: CreateRoleDto) {
+        return this.roleService.create();
+    }
+
+    @Get()
+    findAll() {
+        return this.roleService.findAll();
+    }
+
+    @Get(':id')
+    findOne(param('id') id: string) {
+        return this.roleService.findOne();    
+    }
+
+    @Delete(':id')
+    remove(Param('id') id: string) {
+        this.roleService.remove();
+    }
+}
--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
+import { RolesService } from './roles.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Role } from './role_entities/role.entities';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Role])],
   controllers: [RolesController],
+  providers: [RolesService]
 })
 export class RolesModule {}
